1ae28dc6ab38e55a2c471e487f4a9e79b133b910
JoaoCostaIFG <joaocosta.tra@outlook.com>
2019-07-09 11:28:43 +0100

    Fixed multiple bugs in dtodo related to file names containing blank chars like space.

--- Scripts/dtodo.sh
+++ Scripts/dtodo.sh
@@ -15,25 +15,25 @@ switch_file () {
                 if ! [ "$existance" ]
                 then
 			#Notification sounded like a good idea but they are hidden by dmenu on the top of the screen
-                        touch $notes_path/$notes_file_temp
+                        touch "$notes_path/$notes_file_temp"
 			#notify-send -u low "$notes_file created."
 
                 fi
 
-                notes_file=$notes_file_temp
+                notes_file="$notes_file_temp"
 
         fi
 }
 
 
 #Note folder path
-notes_path=$HOME/Documents/Notes
-notes_file=TODO.txt
+notes_path="$HOME/Documents/Notes"
+notes_file="TODO.txt"
 
 #Create todo.txt file if it doesn't exist
 if ! [ -f $notes_path/$notes_file ]
 then
-	touch $notes_path/$notes_file
+	touch "$notes_path/$notes_file"
 fi
 
 op=1
@@ -41,7 +41,7 @@ op=1
 while [ "$op" ]
 do
 	#dmenu_text=$(cat $notes_path/$notes_file)
-	dmenu_text=$(grep -ve '^[[:space:]]*$' $notes_path/$notes_file)
+	dmenu_text=$(grep -ve '^[[:space:]]*$' "$notes_path/$notes_file")
 	if [ "$dmenu_text" ]
 	then
 		dmenu_text+=$'\n---Switch File---\n---Clean File---\n---Delete File---\n'
@@ -59,10 +59,11 @@ do
         then
 		#Sort and remove blank and duplicate lines from current file
 		tempfile=$(mktemp /tmp/dtodo-script.XXXXXX)
-		grep -ve '^[[:space:]]*$' $notes_path/$notes_file > $tempfile
-		cp $tempfile $notes_path/$notes_file
-		sort -u $notes_path/$notes_file > $tempfile
-		mv $tempfile $notes_path/$notes_file
+		grep -ve '^[[:space:]]*$' "$notes_path/$notes_file" > "$tempfile"
+		cp "$tempfile" "$notes_path/$notes_file"
+
+		sort -u "$notes_path/$notes_file" > "$tempfile"
+		mv "$tempfile" "$notes_path/$notes_file"
 		#notify-send -u low "$notes_file cleaned."
 
 	elif [ "$op" == "---Delete File---" ]
@@ -78,18 +79,18 @@ do
 		
 		fi
 
-	elif grep -Fxq "$op" $notes_path/$notes_file
+	elif grep -Fxq "$op" "$notes_path/$notes_file"
 	then
 		#Remove selected string from file (and it's duplicates)
 		tempfile=$(mktemp /tmp/dtodo-script.XXXXXX)
-		grep -Fvx "$op" $notes_path/$notes_file > $tempfile
-		mv $tempfile $notes_path/$notes_file
+		grep -Fvx "$op" "$notes_path/$notes_file" > "$tempfile"
+		mv "$tempfile" "$notes_path/$notes_file"
 
 	elif [ "$op" ]
 	then
 		#Append new string to file
 		#Doing the same test as the while loop because we don't want to insert empty lines
-		printf "%s\n" "$op" >> $notes_path/$notes_file
+		printf "%s\n" "$op" >> "$notes_path/$notes_file"
 
 	fi
 done
