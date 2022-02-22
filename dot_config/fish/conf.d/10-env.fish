548e7bbda35c8d6f97572af73f631bdc3f1a17fa
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-02-22 18:45:05 +0000

    Update: simplified fish env var parsing.

--- dot_config/fish/conf.d/10-env.fish
+++ dot_config/fish/conf.d/10-env.fish
@@ -1,6 +1,8 @@
 # This will read the file with environment variables and
 # process the lines so they can be run on fish
-for x in (sed 's/\s*#.*$//;/^\s*$/d;' "$HOME/.profile")
-  set xfish (echo $x | sed 's/"\$(/(/g; s/)"/)/g;')
-  eval $xfish
+
+status --is-login || exit
+
+for x in (sed 's/\s*#.*$//; /^\s*$/d; s/"\$(/(/g; s/)"/)/g;' "$HOME/.profile")
+  eval $x
 end
