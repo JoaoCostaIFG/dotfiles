c71ea11583438840b91fc8b09bd5471c0f156d98
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-10-30 00:46:31 +0100

    Use fish vim

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,11 +2,11 @@
 status is-interactive || exit
 
 # Vi
-#fish_vi_key_bindings
-#set fish_escape_delay_ms 10
-## Emulates vim's cursor shape behavior
-#set fish_cursor_default block           # set the normal and visual mode cursors to a block
-#set fish_cursor_insert line             # set the insert mode cursor to a line
-#set fish_cursor_replace underscore      # Set the replace mode cursor to an underscore
-#set fish_cursor_replace_one underscore 
-#set fish_cursor_visual block blink       # Set the visual mode cursor to a blinking block
+fish_vi_key_bindings
+set fish_escape_delay_ms 10
+# Emulates vim's cursor shape behavior
+set fish_cursor_default block           # set the normal and visual mode cursors to a block
+set fish_cursor_insert line             # set the insert mode cursor to a line
+set fish_cursor_replace underscore      # Set the replace mode cursor to an underscore
+set fish_cursor_replace_one underscore 
+set fish_cursor_visual block blink       # Set the visual mode cursor to a blinking block
