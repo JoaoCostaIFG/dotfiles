478ed6d667d39cae97432e5c25f7c6d456a10b53
JoaoCostaIFG <joaocosta.work@posteo.net>
2023-12-28 14:03:56 +0000

    Allacrity lock

---
+++ scripts/backup_root.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eou
+
+backup_drive_part="/dev/sde1"
+
+# mount the drive
+mount "$backup_drive_part" /mnt
+
+# mount the root to get rid of dev, proc, sys, etc. files
+mkdir -p /mnt/tmp
+mount / /mnt/tmp/ -o bind
+
+# rsync the files
+mkdir -p "/mnt/$(hostname)"
+rsync -avPAHXxP --numeric-ids --exclude "/mnt/tmp/mnt" --exclude "/mnt/tmp/mnt" "/mnt/tmp/" "/mnt/$(hostname)"
+
+# sync and umount
+sync
+umount -R /mnt
