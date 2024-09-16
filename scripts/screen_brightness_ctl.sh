9eee5e4359c867630afd9e6014979da6d30c945b
JoaoCostaIFG <joaocosta.work@posteo.net>
2024-09-16 14:34:40 +0100

    Refactor screen brightness control script

--- scripts/screen_brightness_ctl.sh
+++ scripts/screen_brightness_ctl.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
+### Sets the brightness of external monitors.
+### Just pass the number and it sets the same value on all detected monitors.
+### The previous brightness value for each monitor is stored on a cache file and
+### can be restored by the `restore` command. The `lower` command sets all monitors
+### to a pre-defined low value (default 10).
 
 # cache file
 LOCK_FILE="/tmp/screen_brightness.lock"
 CACHE_FILE="$HOME/.cache/ddcutil_brightness_restore"
 LOW_BRIGHTNESS="10"
 
+set_brightness_task() {
+  newBrightness="$1"
+  monitorId="$2"
+
+  currentBrightness="$(ddcutil getvcp 10 -d "$monitorId" | grep -o 'current value = *[0-9]*' | grep -o '[0-9]*')"
+  # save brightness
+  echo "$monitorId-$currentBrightness" >>"$CACHE_FILE"
+  # lower brightness
+  ddcutil setvcp 10 "$newBrightness" -d "$monitorId"
+}
+
 set_brightness() {
+  newBrightness="$1"
+
   # clear cache
   printf "" >"$CACHE_FILE"
 
   ddcutil detect | grep Display | cut -d' ' -f2 | while read -r monitorId; do
-    currentBrightness="$(ddcutil getvcp 10 -d "$monitorId" | grep -o 'current value = *[0-9]*' | grep -o '[0-9]*')"
-    # save brightness
-    echo "$monitorId-$currentBrightness" >>"$CACHE_FILE"
-    # lower brightness
-    ddcutil setvcp 10 "$1" -d "$monitorId"
+    set_brightness_task "$newBrightness" "$monitorId"
   done
 }
 
@@ -35,7 +49,7 @@ case "$1" in
     ddcutil setvcp 10 "$brightness" -d "$monitorId"
   done <"$CACHE_FILE"
   ;;
-[0-9]|[0-9][0-9]|[0-9][0-9][0-9])
+[0-9] | [0-9][0-9] | [0-9][0-9][0-9])
   set_brightness "$1"
   ;;
 *)
