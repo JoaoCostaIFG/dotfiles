30ccf6c3555e8f2f6a5e41ef6174cc104d68a238
JoaoCostaIFG <joaocosta.work@posteo.net>
2024-11-11 21:55:05 +0000

    nvim: add none-ls for codespell

---
+++ dot_config/nvim/lua/plugins/plenary.lua
@@ -0,0 +1,6 @@
+-- Description: plenary: full; complete; entire; absolute; unqualified. All the lua functions I don't want to write twice.
+-- Source: https://github.com/nvim-lua/plenary.nvim
+
+return {
+    "nvim-lua/plenary.nvim"
+}
