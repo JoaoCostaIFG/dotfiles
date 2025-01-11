018b21da3580d2855114eaebe7da6fcf8ae34064
JoaoCostaIFG <joaocosta.work@posteo.net>
2025-01-11 00:13:47 +0000

    colors: playing around with themes

--- dot_config/nvim/lua/plugins/everforest.lua
+++ dot_config/nvim/lua/plugins/everforest.lua
@@ -2,18 +2,18 @@
 -- Source: https://github.com/sainnhe/everforest
 
 return {
-    "sainnhe/everforest",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    init = function()
-        vim.opt.background = "dark"
-        vim.g.everforest_background = "hard"
-        vim.g.everforest_enable_italic = true
-        vim.g.everforest_transparent_background = true
-        vim.g.everforest_diagnostic_line_highlight = true
-    end,
-    config = function()
-        -- load the colorscheme here
-        vim.cmd([[colorscheme everforest]])
-    end,
+	"sainnhe/everforest",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	init = function()
+		vim.opt.background = "dark"
+		vim.g.everforest_background = "hard"
+		vim.g.everforest_enable_italic = true
+		vim.g.everforest_transparent_background = true
+		vim.g.everforest_diagnostic_line_highlight = true
+	end,
+	config = function()
+		-- load the colorscheme here
+		-- vim.cmd([[colorscheme everforest]])
+	end,
 }
