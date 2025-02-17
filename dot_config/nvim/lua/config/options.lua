e26c3073257bb812a75db0b5fc76b3f539696daa
JoaoCostaIFG <joaocosta.work@posteo.net>
2025-02-17 16:09:22 +0000

    nvim: more option tweaks

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -9,10 +9,27 @@ o.scrolloff = 5 -- Lines of context
 o.hlsearch = false
 o.autoindent = true -- Copy indent from current line when starting a new line
 o.listchars:append({ lead = "⋅" })
+-- tab size=2 and use spaces instead of tab chars
 o.ts = 2
 o.sts = 2
 o.sw = 2
 o.expandtab = true
 
+-- auto format on save
+g.autoformat = true
+
 -- disable animations: https://github.com/folke/snacks.nvim/blob/main/docs/animate.md
 g.snacks_animate = false
+
+-- Text behaviour (just don't like comment continuation)
+-- o.formatoptions = o.formatoptions
+--                    + 't'    -- auto-wrap text using textwidth
+--                    + 'c'    -- auto-wrap comments using textwidth
+--                    + 'r'    -- auto insert comment leader on pressing enter
+--                    + 'o'    -- auto insert comment leader on pressing o
+--                    + 'q'    -- format comments with gq
+--                    + 'a'    -- autoformat the paragraphs
+--                    + 'n'    -- autoformat numbered list
+--                    - '2'    -- I am a programmer and not a writer
+--                    + 'j'    -- Join comments smartly
+o.formatoptions = o.formatoptions - { "r", "o" }
