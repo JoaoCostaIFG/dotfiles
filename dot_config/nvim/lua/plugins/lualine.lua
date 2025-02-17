59ba59c91aa5d3bbea8b650743a59d2238f9d5e1
JoaoCostaIFG <joaocosta.work@posteo.net>
2025-02-17 17:01:59 +0000

    nvim: show search count in status line

---
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    table.insert(opts.sections.lualine_x, {
+      function()
+        return require("noice").api.status.search.get()
+      end,
+      cond = function()
+        return package.loaded["noice"] and require("noice").api.status.search.has()
+      end,
+    })
+  end,
+}
