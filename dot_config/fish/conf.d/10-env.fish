e89258348ffe81d9153728f2abfa407023cb06cb
JoaoCostaIFG <joaocosta.work@posteo.net>
2025-07-31 16:38:31 +0100

    theme: use catppuccin for qt and gtk
    Signed-off-by: JoaoCostaIFG <joaocosta.work@posteo.net>

--- dot_config/fish/conf.d/10-env.fish
+++ dot_config/fish/conf.d/10-env.fish
@@ -51,7 +51,7 @@ set -gx LESS -iFMRSX
 #set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 #set -gx SSH_AUTH_SOCK "$HOME/.bitwarden-ssh-agent.sock"
 set -gx FZF_DEFAULT_OPTS "--layout=reverse --height 40%"
-set -gx QT_QPA_PLATFORMTHEME qt6ct
+set -gx QT_QPA_PLATFORMTHEME kvantum
 set -gx COLORTERM truecolor # advertise truecolor
 set -gx BEMENU_OPTS "-i -w -H 34 -p '' --hp 8 --tb '#3b4252' --tf '#d8dee9 --nb '#0b0a18' --nf '#d8dee9' --hb '#3b4252' --hf '#d8dee9'"
 
