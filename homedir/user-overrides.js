c03d79809a8eff1e7c8762651c9011f94862ca8f
JoaoCostaIFG <joaocosta.work@posteo.net>
2025-08-15 13:08:18 +0100

    feat: add preferences to disable autofill and form capture
    Signed-off-by: JoaoCostaIFG <joaocosta.work@posteo.net>

--- homedir/user-overrides.js
+++ homedir/user-overrides.js
@@ -1,2 +1,8 @@
 user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false);
 user_pref("browser.startup.homepage", "https://tail.tail1dfda.ts.net");
+user_pref("signon.autofillForms", false);
+user_pref("signon.firefoxRelay.feature", "disabled");
+user_pref("signon.formlessCapture.enabled", false);
+user_pref("signon.generation.enabled", false);
+user_pref("signon.rememberSignons", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
