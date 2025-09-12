dcb3081b6590802f8277acf9ea9b11e653fc9496
JoaoCostaIFG <me@joaocosta.dev>
2025-09-12 11:34:38 +0100

    feat: update firefox as default browser and fix executable path
    Replace generic browser.desktop with firefox.desktop so firefox shuts up
    about it. Update Arkenfox startup page preference to restore session.
    
    Signed-off-by: JoaoCostaIFG <me@joaocosta.dev>

--- homedir/user-overrides.js
+++ homedir/user-overrides.js
@@ -1,4 +1,5 @@
 user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false);
+user_pref("browser.startup.page", 1);
 user_pref("browser.startup.homepage", "https://tail.tail1dfda.ts.net");
 user_pref("signon.autofillForms", false);
 user_pref("signon.firefoxRelay.feature", "disabled");
