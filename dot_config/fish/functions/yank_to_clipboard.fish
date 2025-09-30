595cf230aca748334c6cc877fddc45274310bfa2
JoaoCostaIFG <me@joaocosta.dev>
2025-10-01 00:07:18 +0100

    fix: comment in fish config
    Signed-off-by: JoaoCostaIFG <me@joaocosta.dev>

--- dot_config/fish/functions/yank_to_clipboard.fish
+++ dot_config/fish/functions/yank_to_clipboard.fish
@@ -1,3 +1,3 @@
-function yank_to_clipboard -d "Insert latest killring entry into system clipboard"
+function yank_to_clipboard -d "Insert latest killring entry into the system clipboard"
     printf "%s" "$fish_killring[1]" | fish_clipboard_copy
 end
