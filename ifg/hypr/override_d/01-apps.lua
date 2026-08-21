88fa6e7cfb9679f235f2bae0080f65241c53288c
JoaoCostaIFG <me@joaocosta.dev>
2026-08-21 14:55:49 +0100

    feat: get rid of syncthingtray
    Signed-off-by: JoaoCostaIFG <me@joaocosta.dev>

--- ifg/hypr/override_d/01-apps.lua
+++ ifg/hypr/override_d/01-apps.lua
@@ -1,10 +1,8 @@
 -- ifg apps
 
 hl.on("hyprland.start", function()
-    -- backups
-    hl.exec_cmd(run_app .. " syncthingtray-qt6 --wait")
-    -- password manager
-    hl.exec_cmd(run_app .. " bitwarden-desktop")
-    -- XPPen drawing table
-    -- hl.exec_cmd(run_app .. ' QT_QPA_PLATFORM="xcb" /usr/lib/pentablet/PenTablet.sh /mini')
+	-- password manager
+	hl.exec_cmd(run_app .. " bitwarden-desktop")
+	-- XPPen drawing table
+	-- hl.exec_cmd(run_app .. ' QT_QPA_PLATFORM="xcb" /usr/lib/pentablet/PenTablet.sh /mini')
 end)
